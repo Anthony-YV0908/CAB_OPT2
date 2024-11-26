3842420f0c043eac74bcdaa02028c4723a857100
Admin <anthony.yuvega@radenta.com>
2024-11-26 10:12:56 +0800

    newer commit

---
+++ artifacts/Application/cablist/script/neptune.Script/Init.js
@@ -0,0 +1,283 @@
+modelTestData.setData({
+    "MyProducts": [{
+        "productId": 1,
+        "name": "Product 1",
+        "description": "Description for Product 1",
+        "price": "332.83",
+        "quantity": 76,
+        "category": "Category 3"
+    }, {
+        "productId": 2,
+        "name": "Product 2",
+        "description": "Description for Product 2",
+        "price": "937.84",
+        "quantity": 92,
+        "category": "Category 3"
+    }, {
+        "productId": 3,
+        "name": "Product 3",
+        "description": "Description for Product 3",
+        "price": "257.22",
+        "quantity": 32,
+        "category": "Category 3"
+    }, {
+        "productId": 4,
+        "name": "Product 4",
+        "description": "Description for Product 4",
+        "price": "534.71",
+        "quantity": 48,
+        "category": "Category 5"
+    }, {
+        "productId": 5,
+        "name": "Product 5",
+        "description": "Description for Product 5",
+        "price": "521.55",
+        "quantity": 85,
+        "category": "Category 5"
+    }, {
+        "productId": 6,
+        "name": "Product 6",
+        "description": "Description for Product 6",
+        "price": "93.82",
+        "quantity": 42,
+        "category": "Category 4"
+    }, {
+        "productId": 7,
+        "name": "Product 7",
+        "description": "Description for Product 7",
+        "price": "419.96",
+        "quantity": 39,
+        "category": "Category 2"
+    }, {
+        "productId": 8,
+        "name": "Product 8",
+        "description": "Description for Product 8",
+        "price": "636.61",
+        "quantity": 52,
+        "category": "Category 2"
+    }, {
+        "productId": 9,
+        "name": "Product 9",
+        "description": "Description for Product 9",
+        "price": "826.42",
+        "quantity": 46,
+        "category": "Category 1"
+    }, {
+        "productId": 10,
+        "name": "Product 10",
+        "description": "Description for Product 10",
+        "price": "428.59",
+        "quantity": 64,
+        "category": "Category 5"
+    }, {
+        "productId": 11,
+        "name": "Product 11",
+        "description": "Description for Product 11",
+        "price": "488.19",
+        "quantity": 5,
+        "category": "Category 2"
+    }, {
+        "productId": 12,
+        "name": "Product 12",
+        "description": "Description for Product 12",
+        "price": "315.64",
+        "quantity": 74,
+        "category": "Category 5"
+    }, {
+        "productId": 13,
+        "name": "Product 13",
+        "description": "Description for Product 13",
+        "price": "753.63",
+        "quantity": 91,
+        "category": "Category 5"
+    }, {
+        "productId": 14,
+        "name": "Product 14",
+        "description": "Description for Product 14",
+        "price": "415.83",
+        "quantity": 99,
+        "category": "Category 3"
+    }, {
+        "productId": 15,
+        "name": "Product 15",
+        "description": "Description for Product 15",
+        "price": "282.19",
+        "quantity": 5,
+        "category": "Category 4"
+    }, {
+        "productId": 16,
+        "name": "Product 16",
+        "description": "Description for Product 16",
+        "price": "192.14",
+        "quantity": 82,
+        "category": "Category 4"
+    }, {
+        "productId": 17,
+        "name": "Product 17",
+        "description": "Description for Product 17",
+        "price": "330.73",
+        "quantity": 94,
+        "category": "Category 2"
+    }, {
+        "productId": 18,
+        "name": "Product 18",
+        "description": "Description for Product 18",
+        "price": "962.86",
+        "quantity": 26,
+        "category": "Category 3"
+    }, {
+        "productId": 19,
+        "name": "Product 19",
+        "description": "Description for Product 19",
+        "price": "108.98",
+        "quantity": 94,
+        "category": "Category 2"
+    }, {
+        "productId": 20,
+        "name": "Product 20",
+        "description": "Description for Product 20",
+        "price": "638.98",
+        "quantity": 40,
+        "category": "Category 4"
+    }, {
+        "productId": 21,
+        "name": "Product 21",
+        "description": "Description for Product 21",
+        "price": "939.56",
+        "quantity": 89,
+        "category": "Category 5"
+    }, {
+        "productId": 22,
+        "name": "Product 22",
+        "description": "Description for Product 22",
+        "price": "494.82",
+        "quantity": 92,
+        "category": "Category 5"
+    }, {
+        "productId": 23,
+        "name": "Product 23",
+        "description": "Description for Product 23",
+        "price": "125.46",
+        "quantity": 72,
+        "category": "Category 4"
+    }, {
+        "productId": 24,
+        "name": "Product 24",
+        "description": "Description for Product 24",
+        "price": "411.29",
+        "quantity": 80,
+        "category": "Category 3"
+    }, {
+        "productId": 25,
+        "name": "Product 25",
+        "description": "Description for Product 25",
+        "price": "78.85",
+        "quantity": 53,
+        "category": "Category 4"
+    }, {
+        "productId": 26,
+        "name": "Product 26",
+        "description": "Description for Product 26",
+        "price": "402.60",
+        "quantity": 28,
+        "category": "Category 4"
+    }, {
+        "productId": 27,
+        "name": "Product 27",
+        "description": "Description for Product 27",
+        "price": "750.03",
+        "quantity": 14,
+        "category": "Category 2"
+    }, {
+        "productId": 28,
+        "name": "Product 28",
+        "description": "Description for Product 28",
+        "price": "629.59",
+        "quantity": 38,
+        "category": "Category 3"
+    }, {
+        "productId": 29,
+        "name": "Product 29",
+        "description": "Description for Product 29",
+        "price": "734.25",
+        "quantity": 7,
+        "category": "Category 4"
+    }, {
+        "productId": 30,
+        "name": "Product 30",
+        "description": "Description for Product 30",
+        "price": "207.55",
+        "quantity": 87,
+        "category": "Category 3"
+    }, {
+        "productId": 31,
+        "name": "Product 31",
+        "description": "Description for Product 31",
+        "price": "621.54",
+        "quantity": 40,
+        "category": "Category 2"
+    }, {
+        "productId": 32,
+        "name": "Product 32",
+        "description": "Description for Product 32",
+        "price": "414.09",
+        "quantity": 54,
+        "category": "Category 1"
+    }, {
+        "productId": 33,
+        "name": "Product 33",
+        "description": "Description for Product 33",
+        "price": "276.74",
+        "quantity": 32,
+        "category": "Category 5"
+    }, {
+        "productId": 34,
+        "name": "Product 34",
+        "description": "Description for Product 34",
+        "price": "667.98",
+        "quantity": 26,
+        "category": "Category 3"
+    }, {
+        "productId": 35,
+        "name": "Product 35",
+        "description": "Description for Product 35",
+        "price": "184.16",
+        "quantity": 30,
+        "category": "Category 2"
+    }, {
+        "productId": 36,
+        "name": "Product 36",
+        "description": "Description for Product 36",
+        "price": "179.80",
+        "quantity": 16,
+        "category": "Category 1"
+    }, {
+        "productId": 37,
+        "name": "Product 37",
+        "description": "Description for Product 37",
+        "price": "329.09",
+        "quantity": 54,
+        "category": "Category 5"
+    }, {
+        "productId": 38,
+        "name": "Product 38",
+        "description": "Description for Product 38",
+        "price": "539.54",
+        "quantity": 90,
+        "category": "Category 3"
+    }, {
+        "productId": 39,
+        "name": "Product 39",
+        "description": "Description for Product 39",
+        "price": "533.56",
+        "quantity": 16,
+        "category": "Category 3"
+    }, {
+        "productId": 40,
+        "name": "Product 40",
+        "description": "Description for Product 40",
+        "price": "591.56",
+        "quantity": 91,
+        "category": "Category 2"
+    }]
+});
