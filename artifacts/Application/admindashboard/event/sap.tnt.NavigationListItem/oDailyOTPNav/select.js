fbbdb509a6cde7672bad7c07470ff7fc89400931
Admin <jade.lapore@radenta.com>
2024-11-26 13:59:46 +0800

    initial commit

--- artifacts/Application/admindashboard/event/sap.tnt.NavigationListItem/oDailyOTPNav/select.js
+++ artifacts/Application/admindashboard/event/sap.tnt.NavigationListItem/oDailyOTPNav/select.js
@@ -1,5 +1,5 @@
-// oApp.to(oDailyOtp);
-// // Load App into parent object (example: sap.m.Page)
-// AppCache.Load("daily_otp", {
-//     parentObject: oDailyOtp
-// });
+oApp.to(oDailyOtp);
+// Load App into parent object (example: sap.m.Page)
+AppCache.Load("daily_otp", {
+    parentObject: oDailyOtp
+});
